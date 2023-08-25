5141743b80d749732fefde5711ae4a01e2de0129
Aryan <aryanrot234@gmail.com>
2023-08-26 05:19:45 +0530

    Updates

---
+++ static/js/180.8ede4641.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunknftifyproject=self.webpackChunknftifyproject||[]).push([[180],{69180:function(e,o,t){t.r(o);var f=null;"undefined"!==typeof WebSocket?f=WebSocket:"undefined"!==typeof MozWebSocket?f=MozWebSocket:"undefined"!==typeof t.g?f=t.g.WebSocket||t.g.MozWebSocket:"undefined"!==typeof window?f=window.WebSocket||window.MozWebSocket:"undefined"!==typeof self&&(f=self.WebSocket||self.MozWebSocket),o.default=f}}]);
+//# sourceMappingURL=180.8ede4641.chunk.js.map
